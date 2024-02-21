a1eb5c4dd7eae0a6b88b6f14024c652a77abb0e5
Fernando <Fernando@DESKTOP-9JIAMII>
2024-02-21 11:04:36 -0600

    Agregar archivos de proyecto.

---
+++ PruebaTecnica02FJMA/Models/Curso.cs
@@ -0,0 +1,17 @@
+﻿namespace PruebaTecnica02FJMA.Models
+{
+    public partial class Curso
+    {
+
+        public Curso()
+        {
+            Estudiantes = new HashSet<Estudiante>();
+        }
+
+        public int CursoId { get; set; }
+        public string Nombre { get; set; } = null!;
+
+        public virtual ICollection<Estudiante> Estudiantes { get; set; }
+
+    }
+}
---
+++ PruebaTecnica02FJMA/Models/Estudiante.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PruebaTecnica02FJMA.Models
+{
+    public class Estudiante
+    {
+        public int EstudianteId { get; set; }
+        public string Nombre { get; set; } = null!;
+        public byte[]? Imagen { get; set; }
+        [Display(Name = "Curso")]
+        public int CursoId { get; set; }
+
+        public virtual Curso Curso { get; set; } = null!;
+    }
+}
